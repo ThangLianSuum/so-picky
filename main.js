e0cf1921eeb18e1efde27ec006a03fb5194d856d
ThangLianSuum <75762682+ThangLianSuum@users.noreply.github.com>
2021-01-31 09:03:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var inputs = [];
+
+function getparagraph1()
+{
+    for(var i = 1 <=6 ; i++)
+    {
+        inputs.push(document.getElementById("para1_input_box_" + i).value);
+        inputs.join(". ");
+        document.getElementById("showparagraph1").innerHTML = inputs.join(". ");
+    }
+}
+function getparagraph2()
+{
+    for(var i = 1 <=6 ; i++)
+    {
+        inputs.push(document.getElementById("para1_input_box_" + i).value);
+        inputs.join(". ");
+        document.getElementById("showparagraph1").innerHTML = inputs.join(". ");
+    }
+}
